c44a7654deb1551f42bf68e9300e2a70ef89b795
Kennadee <kennadee.ray@gmail.com>
2020-08-12 17:00:16 -0500

    removed hard coded port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { animals } = require('./data/animals');
-var express = require('express');
-var app = express();
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -37,8 +38,8 @@ function filterByQuery(query, animalsArray) {
   }
   res.json(results);
    });
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
     
   });
   
