e60bd35fde3e3f96cd3b43540304a79b20d14a22
Kennadee <kennadee.ray@gmail.com>
2020-08-13 11:52:28 -0500

    get routes added to other pages

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 //parse incoming JSON data
 app.use(express.json());
+// allows us to use the front end styles and javascript
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -77,6 +79,15 @@ app.post('/api/animals', (req, res) => {
     } else {
       res.send(404);
     }
+  });
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
   });
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
